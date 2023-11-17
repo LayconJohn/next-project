f67ce190b969e94588605435bd66d3d8713c795b
Laycon John <laycon.gomes@tld.local>
2023-11-17 10:58:43 -0300

    style(home): create card model

--- birth-date/src/app/page.tsx
+++ birth-date/src/app/page.tsx
@@ -1,11 +1,24 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import image from "../../public/we.jpeg"
 
 export default function Home() {
   return (
     <main className={styles.main}>
 
-
+      <div className={`${styles.card}`}>
+        <h2>teste</h2>
+        <Image 
+          src={image}
+          alt="A gente"
+          width={100}
+          height={100}
+          objectFit='cover'
+          objectPosition='center'
+        />
+        <p>teste2</p>
+        <span>teste3</span>
+      </div>
 
 
 
